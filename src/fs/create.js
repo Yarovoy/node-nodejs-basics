286236905860b9903b8fa46d92a8b6d32f5ebc75
Yuriy Yarovoy <yy@ohmy.cloud>
2022-12-04 15:31:32 +0100

    feat: implement `create.js`

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,25 @@
+import { access, writeFile } from 'node:fs/promises';
+
+const ifFileExists = async (filePath) => {
+  let fileExist = true;
+
+  try {
+    await access(filePath);
+  } catch (error) {
+    fileExist = false;
+  }
+
+  return fileExist;
+};
+
 const create = async () => {
-    // Write your code here 
+  const filePath = './files/fresh.txt';
+
+  if (await ifFileExists(filePath)) {
+    throw new Error('FS operation failed');
+  }
+
+  await writeFile(filePath, 'I am fresh and young');
 };
 
 await create();
