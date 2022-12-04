f5d798b6647bac9f2c5ff30becca83c502554ea3
Yuriy Yarovoy <yy@ohmy.cloud>
2022-12-04 20:56:10 +0100

    refactor: change path to file to the project-relative

--- src/fs/create.js
+++ src/fs/create.js
@@ -3,7 +3,7 @@ import { writeFile } from 'node:fs/promises';
 import { existsAsync } from './helpers/existsAsync.js';
 
 const create = async () => {
-  const filePath = './files/fresh.txt';
+  const filePath = './src/fs/files/fresh.txt';
 
   if (await existsAsync(filePath)) {
     throw new Error('FS operation failed');
