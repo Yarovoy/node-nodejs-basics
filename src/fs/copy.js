5ba64efae2c54f895389836f3a72f3ac6f26aa5a
Yuriy Yarovoy <yy@ohmy.cloud>
2022-12-04 20:57:49 +0100

    feat: implement `fs/copy.js` logic

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,23 @@
+import { cp } from 'node:fs/promises';
+
+import { existsAsync } from './helpers/existsAsync.js';
+
 const copy = async () => {
-    // Write your code here 
+  const srcPath = './src/fs/files';
+  const destPath = './src/fs/files_copy';
+
+  if (!await existsAsync(srcPath)) {
+    throw new Error('FS operation failed');
+  }
+
+  try {
+    await cp(srcPath, destPath, { recursive: true, errorOnExist: true, force: false });
+  } catch (error) {
+    if (error.code === 'ERR_FS_CP_EEXIST') {
+      throw new Error('FS operation failed');
+    }
+  }
+
 };
 
-copy();
+await copy();
